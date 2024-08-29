2facea46851613a0e22726970da57b35b7a6696d
CarlUdo <carl@wbsweden.com>
2024-08-29 19:27:37 +0200

    Add additional tests

--- precourse/nodejs-training-ground/tests.js
+++ precourse/nodejs-training-ground/tests.js
@@ -76,3 +76,50 @@ describe('age classifier', () => {
     assert.equal(result, 'probably dead')
   });
 });
+
+describe('constants and variables', () => {
+  it.skip('const means constant', () => {
+    // arrange
+    const aNumber = 1;
+    aNumber = 2;
+
+    // assert
+    assert.equal(result, )
+  });
+
+  it('you can change a let variable', () => {
+    // arrange
+    let aNumber = 1;
+  
+    // act
+    aNumber = 2;
+  
+    // assert
+    assert.equal(aNumber, 2);
+  });
+
+  it('uninitialized variables are undefined', () => {
+    // act
+    let aNumber;
+  
+    // assert
+    assert.equal('undefined', typeof aNumber);
+    assert.equal(undefined, aNumber);
+  });
+
+  it('null should be used for not set', () => {
+    // arrange
+    // declaring a dummy function
+    const peekIntoTheBox = () => {
+      return false;
+    };
+  
+    // act
+    let isCatAlive = null;
+  
+    // assert
+    assert.equal(null, isCatAlive);
+    isCatAlive = peekIntoTheBox();
+    assert.equal(false, isCatAlive);
+  });
+});

bfa9ccce3bf4dd6e1cff6a1547ea3d826449af94
CarlUdo <carl@wbsweden.com>
2024-09-12 18:32:43 +0200

    Read html page in memory

--- precourse/web-api-js-training-ground/index.js
+++ precourse/web-api-js-training-ground/index.js
@@ -1,9 +1,11 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end('Hello fellow developer!')
+  const htmlPage = fs.readFileSync('./static/index.html', { encoding: 'utf-8'});
+  res.end(htmlPage);
 });
 
 const hostName = 'localhost';

e63f925d5e7a1d410520f630d24e41307d3ceb70
CarlUdo <carl@wbsweden.com>
2024-09-17 19:53:06 +0200

    Make a dynamic route that returns correct user from database

--- precourse/express-server/index.js
+++ precourse/express-server/index.js
@@ -17,8 +17,30 @@ const db = [
   },
 ];
 
+const getUser = id => {
+  for (let i = 0; i < db.length; i++) {
+    if (id === db[i].id) {      
+      return db[i];
+    }  
+  }
+
+  return {
+    error: `No user with id ${id} exists in database.`
+  };
+};
+
 app.get('/api/developers', (req, res) => {
   res.json(db);
+});
+
+app.get('/api/developers/:id', (req, res) => {
+  const { id } = req.params;
+
+  const user = getUser(Number(id));
+
+  const statusCode = user.error ? 404 : 200;
+
+  res.status(statusCode).json(user);
 });
 
 app.listen(port, hostName, () => {

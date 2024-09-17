0b82f3dcdb853b7cfc83951db4430838c03e3bb7
CarlUdo <carl@wbsweden.com>
2024-09-17 11:06:03 +0200

    Serve static files in static folder

--- precourse/express-server/index.js
+++ precourse/express-server/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 const hostName = 'localhost';
+
+// Serve static files from the 'static' directory
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.send('Hello fellow developer!');
 });
 
 app.listen(port, hostName, () => {
-  console.log(`Server running at http://${hostName}/${port}/`);
+  console.log(`Server running at http://${hostName}:${port}/`);
 });

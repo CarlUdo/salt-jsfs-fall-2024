86a826a6c4bb1ea9f88f72d0985b92f3a813c778
CarlUdo <carl@wbsweden.com>
2024-09-17 10:19:48 +0200

    First simple express server

--- precourse/express-server/index.js
+++ precourse/express-server/index.js
@@ -1,1 +1,13 @@
-console.log('Hello from index.js');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+const hostName = 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('Hello fellow developer!');
+});
+
+app.listen(port, hostName, () => {
+  console.log(`Server running at http://${hostName}/${port}/`);
+});

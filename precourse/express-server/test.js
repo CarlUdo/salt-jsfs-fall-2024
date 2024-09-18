a69d43f13e75da5f83ce78754aa65ed16d077ba8
CarlUdo <carl@wbsweden.com>
2024-09-18 19:42:08 +0200

    Write test for POST a new user

--- precourse/express-server/test.js
+++ precourse/express-server/test.js
@@ -7,7 +7,7 @@ describe('developer API should have endpoints too', () => {
     // act and assert
     request(app)
       .get('/api/developers/')
-      .set('Application', 'application/json')
+      .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(res => strictEqual(res.body.length, 2))
       .expect(200, done);
@@ -16,9 +16,23 @@ describe('developer API should have endpoints too', () => {
   it('get user with id 1', done => {
     request(app)
       .get('/api/developers/1')
-      .set('Application', 'application/json')
+      .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(res => strictEqual(res.body.name, 'John Doe'))
       .expect(200, done);
   });
+
+  it('create new user', done => {
+    request(app)
+      .post('/api/developers/')
+      .send({
+        "name": 'Carl',
+        "email": 'c@b.com'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect('Location', /\/api\/developers\/3/)
+      .expect(res => strictEqual(res.body.name, 'Carl'))
+      .expect(201, done)
+  })
 });

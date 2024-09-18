c35f5f150a967bae7dbc9eb0431a8c392d5ecfa9
CarlUdo <carl@wbsweden.com>
2024-09-18 19:06:19 +0200

    Write test for GET all users

--- precourse/express-server/test.js
+++ precourse/express-server/test.js
@@ -1,13 +1,15 @@
 import { strictEqual }from 'assert';
 import request from 'supertest';
+import { app } from './api.js'
 
 describe('developer API should have endpoints too', () => {
-  it('get all developers', () => {
-    // arrange
-
-    // act
-
-    // assert
-    strictEqual(true, false);
+  it('get all developers', done => {
+    // act and assert
+    request(app)
+      .get('/api/developers/')
+      .set('Application', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(res => strictEqual(res.body.length, 2))
+      .expect(200, done);
   });
 });

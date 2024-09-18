ea20816049da0c9dc5a8a98e1ea185a52b833693
CarlUdo <carl@wbsweden.com>
2024-09-18 18:27:38 +0200

    Create a test.js file make a test template

---
+++ precourse/express-server/test.js
@@ -0,0 +1,13 @@
+import { strictEqual }from 'assert';
+import request from 'supertest';
+
+describe('developer API should have endpoints too', () => {
+  it('get all developers', () => {
+    // arrange
+
+    // act
+
+    // assert
+    strictEqual(true, false);
+  });
+});

5919353aa4f0864c24481555f4539f64fcbbbafd
CarlUdo <carl@wbsweden.com>
2024-09-19 14:08:53 +0200

    Make a simple Express server template

---
+++ precourse/address-book/dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+// import express from 'express';
+// import { Request, Response } from 'express';
+Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express();
+// const host = '0.0.0.0';
+// const port = 3000;
+// app.get('/', (req: Request, res: Response) => {
+//   res.send(`<h1>Welocme to this address application!</h1>`);
+// });
+// app.listen(port, host, () => 
+//   console.log(`Server is listening to http://${host}/${port}`));
+console.log('Hmmm');
+//# sourceMappingURL=app.js.map
--- precourse/address-book/src/app.ts
+++ precourse/address-book/src/app.ts
@@ -1,1 +1,14 @@
-console.log('Testar...');
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+const host = '0.0.0.0';
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<h1>Welcome to this address application!</h1>`);
+});
+
+app.listen(port, host, () => 
+  console.log(`Server is listening to http://${host}/${port}`));

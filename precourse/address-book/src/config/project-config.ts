d883933d82bdc2c6d8444318f7c0d91fa034bbd4
CarlUdo <carl@wbsweden.com>
2024-09-20 12:04:00 +0200

    Make a project config file

---
+++ precourse/address-book/src/config/project-config.ts
@@ -0,0 +1,7 @@
+import * as path from 'path';
+
+const configPath = path.resolve(__dirname);
+
+export const PROJECT_CONFIG = {
+  rootPath: path.dirname(path.dirname(configPath)) // Move up two levels in the directory structure
+};

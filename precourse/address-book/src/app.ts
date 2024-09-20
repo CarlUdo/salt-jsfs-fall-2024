4fd4e99dd7b7ac26483b8a5e0ea20179e99a0815
CarlUdo <carl@wbsweden.com>
2024-09-20 14:45:01 +0200

    Make first route and router and return all users

--- precourse/address-book/src/app.ts
+++ precourse/address-book/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import { generateInMemoryDatabase } from './utils/generate-in-memory-database';
+import { usersRouter } from './routes/users';
 
 generateInMemoryDatabase(100); // Generates a db for development purposes
 
@@ -9,9 +9,7 @@ const app = express();
 const host = '0.0.0.0';
 const PORT = Number(process.env.LOCAL_HOST_PORT) || 4001;
       
-app.get('/', (req: Request, res: Response) => {
-  res.send(`<h1>Welcome to this address application!</h1>`);
-});
+app.use('/users', usersRouter);  
 
 app.listen(PORT, host, () => 
   console.log(`Server is listening to http://${host}/${PORT}`));
---
+++ precourse/address-book/src/controllers/usersController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import { db } from '../database/people-database';
+
+export const getAllUsers = ((req:Request, res:Response) => {
+  res.json(db);
+}); 

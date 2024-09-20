62f9965b4e2d6203380768bfeeef52c8d83c019c
CarlUdo <carl@wbsweden.com>
2024-09-20 12:18:28 +0200

    Generate an in memory database and write to file

--- precourse/address-book/src/app.ts
+++ precourse/address-book/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import { generateInMemoryDatabase } from './utils/generate-in-memory-database';
+
+const db = generateInMemoryDatabase(100);
 
 const app = express();
 
 const host = '0.0.0.0';
 const port = 3000;
-
+      
 app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>Welcome to this address application!</h1>`);
 });
--- precourse/address-book/src/utils/generate-in-memory-database.ts
+++ precourse/address-book/src/utils/generate-in-memory-database.ts
@@ -1,8 +1,17 @@
-import { generatePeople } from "./generate-people"
+import { generatePeople } from "./generate-people";
+import { writeFileSync } from 'fs'
+import { PROJECT_CONFIG } from "../config/project-config";
+
+const { rootPath } = PROJECT_CONFIG;
 
 export const generateInMemoryDatabase = (numberOfPersons: number) => {
   const db = generatePeople(numberOfPersons);
 
-  // write database to file
-  
+  const dbJsonString = JSON.stringify(db, null, 2);
+
+  try {
+    writeFileSync(`${rootPath}/src/database/people-database.ts`, dbJsonString, 'utf8');
+  } catch (err) {
+    console.log(`An error occured while trying to write "database" to file: ${err}`);
+  }  
 };

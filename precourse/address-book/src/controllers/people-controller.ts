29292516467cefd0edbf8ce184e1104683f26aea
CarlUdo <carl@wbsweden.com>
2024-09-21 18:47:10 +0200

    Make route for finding person in database

--- precourse/address-book/src/controllers/people-controller.ts
+++ precourse/address-book/src/controllers/people-controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express';
 import { db } from '../database/people-database';
+import { findPersonInDb } from '../utils/db-helpers';
 
 export const getPeople = ((req: Request, res: Response) => {
   res.json(db); 
 });
-  
+
+export const getPerson = ((req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = findPersonInDb(id);
+
+  const statusCode = user ? 200 : 404;
+
+  res.status(statusCode).json(statusCode === 200 ? user : { error: `No user with id ${id} exists in database.` });  
+});  

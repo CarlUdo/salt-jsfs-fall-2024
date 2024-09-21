0c6f540688eb2074c1ce1761e77bc6306e45e7a0
CarlUdo <carl@wbsweden.com>
2024-09-21 17:21:41 +0200

    Serve static files from static folder

--- precourse/address-book/src/app.ts
+++ precourse/address-book/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { usersRouter } from './routes/users';
 import { errorHandler } from './middlewares/error-handler';
 import morgan from 'morgan';
+import path from 'path';
 // Uncomment below to generate a database for development purposes
 /* import { generateInMemoryDatabase } from './utils/generate-in-memory-database';
 generateInMemoryDatabase(100);  */ 
@@ -14,6 +15,8 @@ const PORT = Number(process.env.LOCAL_HOST_PORT) || 4001;
 app.use(morgan('tiny'));
 
 app.use(express.json({ limit: '5mb' }));
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/users', usersRouter);  
 

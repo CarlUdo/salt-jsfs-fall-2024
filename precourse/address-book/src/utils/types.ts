06f5d3147c0ff663131d0da72b5a6c35c2bcbe07
CarlUdo <carl@wbsweden.com>
2024-09-23 18:35:12 +0200

    Make additional type

--- precourse/address-book/src/utils/types.ts
+++ precourse/address-book/src/utils/types.ts
@@ -26,17 +26,10 @@ export type CreatedPerson = Person & {
   userId: string; 
 };
 
-
-export type Personss = {
-  fullName: string;
-  email: string;
-  address: string;
-  phoneNumber?: string;
-  age: number;
-  gender: string;
-  zodiacSign: string;
-  favoriteDish: string;
-  pet: string;
-  bio: string;  
-  avatar: string;
+export type ErrorInfo = {
+  status: number;
+  name: string;
+  message: string;
+  type: string;
+  stack: string;
 };

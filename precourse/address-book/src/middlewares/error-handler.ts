6b85a895527718e5b7178c5c5387c7cb98743587
CarlUdo <carl@wbsweden.com>
2024-09-23 15:04:09 +0200

    Add try/catch to errorhandler middleware

--- precourse/address-book/src/middlewares/error-handler.ts
+++ precourse/address-book/src/middlewares/error-handler.ts
@@ -3,9 +3,13 @@ import { getErrorInformation } from "../utils/get-error-information";
 
 // Note! Even though next function isn't used it must come as a parameter or strange behaviour will occur
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  const errorInformation = getErrorInformation(err);
+  try {
+    const errorInformation = getErrorInformation(err);
 
-  console.log(errorInformation);
+    console.log(errorInformation);
 
-  res.status(errorInformation.status).json({ message: errorInformation.message });
+    res.status(errorInformation.status).json({ message: errorInformation.message });
+  } catch (err) {
+    console.log(`Error within errorHandler: `, err);
+  }  
 };

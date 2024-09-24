4bfe5388f514eac65ecf0c9ee7411f5f12ba3da1
CarlUdo <carl@wbsweden.com>
2024-09-24 09:24:16 +0200

    Make a test for GET all persons

--- precourse/address-book/src/app.spec.ts
+++ precourse/address-book/src/app.spec.ts
@@ -1,8 +1,14 @@
 import { strictEqual } from "assert";
-import supertest from "supertest";
+import request from "supertest";
+import { app } from "./api";
 
 describe('Test the people api to the limits...', () => {
-  it('This is just a dummy test', () => {
-    strictEqual(true, true);
+  it('get all persons', done => {
+    request(app)
+      .get('/api/people/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(res => strictEqual(res.body.length, 100))
+      .expect(200, done);
   });
 });

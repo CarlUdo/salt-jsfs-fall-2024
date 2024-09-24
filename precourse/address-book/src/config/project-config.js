42e014cad024e680b8aedd42b31d86b3d8e73e8e
CarlUdo <carl@wbsweden.com>
2024-09-24 08:28:16 +0200

    Remove .js files from src folder

--- precourse/address-book/src/config/project-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PROJECT_CONFIG = void 0;
-var path = require("path");
-var configPath = path.resolve(__dirname);
-exports.PROJECT_CONFIG = {
-    rootPath: path.dirname(path.dirname(configPath)), // Move up two levels in the directory structure
-};

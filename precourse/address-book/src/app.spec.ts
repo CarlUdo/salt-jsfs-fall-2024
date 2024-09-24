5d72837210339a27a76367c3fc280b8c179a6613
CarlUdo <carl@wbsweden.com>
2024-09-24 08:55:06 +0200

    Set up a first dummy test

--- precourse/address-book/src/app.spec.ts
+++ precourse/address-book/src/app.spec.ts
@@ -1,1 +1,8 @@
-console.log('Testing testing...');
+import { strictEqual } from "assert";
+import supertest from "supertest";
+
+describe('Test the people api to the limits...', () => {
+  it('This is just a dummy test', () => {
+    strictEqual(true, true);
+  })
+});

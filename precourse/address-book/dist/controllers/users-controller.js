2002ec2fcd49affff6f75e4169c663e64eba7210
CarlUdo <carl@wbsweden.com>
2024-09-25 16:52:22 +0200

    Stop tracking dist/ folder

--- precourse/address-book/dist/controllers/users-controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllUsers = void 0;
-const people_database_1 = require("../database/people-database");
-exports.getAllUsers = ((req, res) => res.json(people_database_1.db));
-//# sourceMappingURL=users-controller.js.map

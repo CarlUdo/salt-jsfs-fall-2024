10f5cab84cc0c80dd7088bfe5dc4cf9e3aee5309
CarlUdo <carl@wbsweden.com>
2024-09-25 16:53:36 +0200

    Remove trailing comma

--- precourse/address-book/src/config/project-config.ts
+++ precourse/address-book/src/config/project-config.ts
@@ -4,5 +4,5 @@ import { fileURLToPath } from 'url';
 const configPath = path.dirname(fileURLToPath(import.meta.url));
 
 export const PROJECT_CONFIG = {
-  rootPath: path.dirname(path.dirname(configPath)), // Move up two levels in the directory structure
+  rootPath: path.dirname(path.dirname(configPath)) // Move up two levels in the directory structure
 };

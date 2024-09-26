5375c7d3cf4948e3b4a7a56a91131402d8a0a32d
CarlUdo <carl@wbsweden.com>
2024-09-26 09:28:39 +0200

    Update config file for how to resolve directory names

--- precourse/address-book/src/config/project-config.ts
+++ precourse/address-book/src/config/project-config.ts
@@ -1,7 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import * as path from 'path';
 
-const configPath = path.dirname(fileURLToPath(import.meta.url));
+const configPath = path.resolve(__dirname);
 
 export const PROJECT_CONFIG = {
   rootPath: path.dirname(path.dirname(configPath)) // Move up two levels in the directory structure

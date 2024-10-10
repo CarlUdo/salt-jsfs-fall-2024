25d237c51cce7525d7ed752c1c760e407fc93386
CarlUdo <carl@wbsweden.com>
2024-10-10 11:23:17 +0200

    Add additional test

--- maincourse/class-live-coding/src/2024-10-10/jsdom.test.ts
+++ maincourse/class-live-coding/src/2024-10-10/jsdom.test.ts
@@ -8,11 +8,33 @@ test("select paragraph and access", () => {
   equal(dom.window.document.querySelector('p')!.textContent, 'Hi!');
 });
 
-test("", () => {
+test("select all p elements", () => {
   const dom = new JSDOM(`<!DOCTYPE html><p>Hi!</p><p>Hidyhow!</p>`);
 
   const result = dom.window.document.querySelectorAll('p').length;
 
   deepEqual(result, 2);
+});
+
+test("see if class name 'link' exists", () => {
+  const dom = new JSDOM(`<!DOCTYPE html><p class="link"></p>`);
+
+  const result = dom.window.document.getElementsByClassName('link')[0].className;
+
+  equal(result, 'link');
+});
+
+test("add class name 'coder'", () => {
+  const dom = new JSDOM(`<!DOCTYPE html><p id="text"></p>` );
+
+  const textElement = dom.window.document.getElementById("text");
+
+  textElement!.classList.add('coder');
+
+  dom.window.document.body.appendChild(textElement!);
+
+  const result = dom.window.document.getElementsByClassName('coder')[0].className;
+
+  deepEqual(result, 'coder');
 });
 

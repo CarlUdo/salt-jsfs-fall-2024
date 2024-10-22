f0302db5740224bc01c8860cbb81e4e50f47e7b8
CarlUdo <carl@wbsweden.com>
2024-10-22 08:57:57 +0200

    Doing som formatting

--- maincourse/react-button-component/src/App.tsx
+++ maincourse/react-button-component/src/App.tsx
@@ -5,4 +5,4 @@ function App() {
   return <ChangeColorButton />
 }
 
-export default App
+export default App;
--- maincourse/react-button-component/src/component/buttons/change-color-button.tsx
+++ maincourse/react-button-component/src/component/buttons/change-color-button.tsx
@@ -2,7 +2,6 @@ import { getRandomColor } from '../../libs/get-random-color';
 import styles from './button.module.css';
 
 export function ChangeColorButton() {
-
   const handleClick = () => {
     document.body.style.backgroundColor = getRandomColor();    
   }
@@ -10,9 +9,9 @@ export function ChangeColorButton() {
   return (
     <button 
       className={styles.button}
-
-      onClick={handleClick}>    
+      onClick={handleClick}
+    >    
       Change background color
     </button>
-  )
+  );
 }
